c28841383c6778cb0eb471470732615c7f594c27
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-19 13:26:32 +0700

    tambahan

--- src/LatihanControlComponent.jsx
+++ src/LatihanControlComponent.jsx
@@ -2,13 +2,17 @@ import React, {useState} from 'react'
 
 function LatihanControlComponent() {
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("");
   return (
     <div className="flex flex-col">
       <h1 className='text-3xl font-light'>Login</h1>
       <form className="flex flex-col gap-4 p-4 m-5">
-        <input type="text" name="username" className="border border-gray-300" placeholder="Username"/>
-        <input type="password" name="password"className="border border-gray-300" placeholder="Password" />
+        <input type="text" name="username" className="border border-gray-300" placeholder="Username" value={username} onChange={(event) => 
+        {setUsername(event.target.value);}}/>
+        <p>{username}</p>
+        <input type="password" name="password"className="border border-gray-300" placeholder="Password" value={password} onChange={(event) => 
+        {setPassword(event.target.value);}}/>
+        <p>{password}</p>
         <button type="submit" className="bg-yellow-500 text-black font-bold px-4 py-2 rounded-lg">Login</button>
       </form>
     </div>
