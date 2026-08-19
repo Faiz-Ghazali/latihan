79fd2da23a5890476eee38eda3d09519e4553fbe
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-19 13:02:57 +0700

    untuk latihan

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LatihanUseRef from './LatihanUseRef.jsx'
+import LatihanControlComponent from './LatihanControlComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LatihanUseRef />
+    <LatihanControlComponent />
   </StrictMode>
 )
