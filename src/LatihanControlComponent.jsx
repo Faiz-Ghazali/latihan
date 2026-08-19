269879c2ba78864f68e4c947011dddb299b7c78b
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-19 13:31:40 +0700

    super

--- src/LatihanControlComponent.jsx
+++ src/LatihanControlComponent.jsx
@@ -3,10 +3,15 @@ import React, {useState} from 'react'
 function LatihanControlComponent() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const handlerSubmit = (event) => {
+      event.preventDefault();
+      console.log(event.target.elements.Username.value);
+      console.log(event.target.elements.password.value);
+    }
   return (
     <div className="flex flex-col">
       <h1 className='text-3xl font-light'>Login</h1>
-      <form className="flex flex-col gap-4 p-4 m-5">
+      <form onSubmit={handlerSubmit} className="flex flex-col gap-4 p-4 m-5">
         <input type="text" name="username" className="border border-gray-300" placeholder="Username" value={username} onChange={(event) => 
         {setUsername(event.target.value);}}/>
         <p>{username}</p>
