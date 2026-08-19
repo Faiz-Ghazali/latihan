40f56b57587bb6efc263b5a596e7e17e8db4b84f
Faiz-Ghazali <faizghazali1.rw1@gmail.com>
2026-08-19 13:06:02 +0700

    fixing

--- src/LatihanControlComponent.jsx
+++ src/LatihanControlComponent.jsx
@@ -4,9 +4,9 @@ function LatihanControlComponent() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("")
   return (
-    <div>
+    <div className="flex flex-col">
       <h1 className='text-3xl font-light'>Login</h1>
-      <form className="flex flex-row gap-4 p-4 m-5">
+      <form className="flex flex-col gap-4 p-4 m-5">
         <input type="text" name="username" className="border border-gray-300" placeholder="Username"/>
         <input type="password" name="password"className="border border-gray-300" placeholder="Password" />
         <button type="submit" className="bg-yellow-500 text-black font-bold px-4 py-2 rounded-lg">Login</button>
